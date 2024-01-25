d0ffcb7ace931ddd1ad956e688fce17619cb53cb
Ayeni kehinde Oluwatosin <42803575+Ayenko4all@users.noreply.github.com>
2024-01-26 00:43:27 +0100

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\ProductSortingRequest;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use App\Services\ProductSortingService;
